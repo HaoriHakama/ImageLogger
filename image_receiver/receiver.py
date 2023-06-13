66d21764fd52b3e57949cc5c34d214963e657d05
DESKTOP2022\h195m <h195mic@gmail.com>
2023-06-13 18:00:15 +0900

    Receiverの作成

--- image_receiver/receiver.py
+++ image_receiver/receiver.py
@@ -1,22 +1,33 @@
-import socket
-import json
+import socketserver
+import base64
 from queue import Queue
-from threading import Thread
 
 
-class Receiver:
-    def __init__(self) -> None:
-        self.q = Queue()
+class ImageReceiverHandler(socketserver.BaseRequestHandler):
+    def handle(self) -> None:
+        image_data_size = int.from_bytes(self.request.recv(4), byteorder="big")
+        image_data = self.receive_data(image_data_size)
+        image_data = base64.b64decode(image_data)
 
-    def start_receiver(self):
-        Thread(target=self._receive_message, daemon=True).start()
+        text_size = int.from_bytes(self.request.recv(4), byteorder="big")
+        text = self.receive_data(text_size).decode()
 
-    def _receive_message(self):
-        soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        soc.bind(("localhost"), 9008)
+        self.server.received_data_q.put([image_data, text])
 
-        while True:
-            data, _ = soc.recvfrom(2048)
-            message = data.decode("utf-8")
-            message = json.loads(message)
-            self.q.put(message)
+    def receive_data(self, size):
+        data = b""
+        while len(data) < size:
+            more_data = self.request.recv(size - len(data))
+            if not more_data:
+                raise Exception("Fail to receive all data")
+            data += more_data
+        return data
+
+    class Receiver(socketserver.TCPServer):
+        def __init__(
+            self, server_address, RequestHandlerClass, bind_and_activate=True
+        ):
+            super().__init__(
+                server_address, RequestHandlerClass, bind_and_activate
+            )
+            self.received_data_q = Queue()
